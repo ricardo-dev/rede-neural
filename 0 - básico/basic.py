c98246f8c5de846a3f448484fd1178abf55735b0
ricardo-dev <ricardopuc.computacao@gmail.com>
2021-09-04 12:01:14 -0300

    primeiras alteracoes

---
+++ 0 - básico/basic.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  4 11:58:39 2021
+
+@author: ricardo
+"""
+
+def testeFuncao(a, b=20):
+    return a*b;
+
+result = testeFuncao(2);
+
+print("Result: ",result);
+
