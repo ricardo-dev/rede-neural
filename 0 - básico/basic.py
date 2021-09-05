25be5f984fda95b2124e79911ad860712b63b1ee
ricardo-dev <ricardopuc.computacao@gmail.com>
2021-09-04 22:19:01 -0300

    [ADD] alterando arquivo basico

--- 0 - básico/basic.py
+++ 0 - básico/basic.py
@@ -3,12 +3,11 @@
 Created on Sat Sep  4 11:58:39 2021
 
 @author: ricardo
+
+basico: https://www.treinaweb.com.br/blog/principais-estruturas-de-dados-no-python
+
+classes: https://algoritmosempython.com.br/cursos/programacao-python/classes-objetos/
 """
 
-def testeFuncao(a, b=20):
-    return a*b;
 
-result = testeFuncao(2);
-
-print("Result: ",result);
 
